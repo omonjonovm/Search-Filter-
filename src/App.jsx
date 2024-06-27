f91179c66d80bcd441245b475cba09ad35bce262
omonjonovm <omonjonovmurodjon2@gmail.com>
2024-06-27 21:19:40 +0500

    d17

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,14 @@ const App = () => {
         <h1 className='text-center mt-4'>Contact Keeper</h1>
         <Form>
           <InputGroup className='my-3'>
-            <Form.Control 
-            onChange={(e) => setSearch(e.target.value)}
-            placeholder='Search contacts' />
+            <Form.Control
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder='Search contacts' />
           </InputGroup>
         </Form>
         <Table striped bordered hover>
           <thead>
             <tr>
-              <th>No_</th>
               <th>First Name</th>
               <th>Last Name</th>
               <th>Email</th>
@@ -29,13 +28,15 @@ const App = () => {
           </thead>
           <tbody>
 
-            {data.map((item) => (
+            {data.filter((item) => {
+              return search.toLocaleLowerCase() === '' ? item
+                : item.first_name.toLocaleLowerCase().includes(search); 
+            }).map((item) => (
               <tr key={item.id}>
-                <td>{item.id}</td>
                 <td>{item.first_name}</td>
                 <td>{item.last_name}</td>
                 <td>{item.email}</td>
-                <td><span>+1 </span>{item.phone}</td>
+                <td>{item.phone}</td>
               </tr>
             ))}
 
